5ee7b373c8b6de2008f06de83a61758e8023d22e
yoroo-237 <ayunirodrick9@gmail.com>
2025-06-16 08:34:56 +0100

    Update server.js

--- Backend/node_server/server.js
+++ Backend/node_server/server.js
@@ -108,4 +108,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Backend on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 port:${PORT}`));
