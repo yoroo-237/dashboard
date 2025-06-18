cd572eede8a52b6400dad9d7f5a2d85676249fa8
yoroo-237 <ayunirodrick9@gmail.com>
2025-06-18 07:39:42 +0100

    Delete utils directory

--- utils/telegram.js
+++
@@ -1,4 +0,0 @@
-// backend/utils/telegram.js
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: false });
-module.exports = bot;
